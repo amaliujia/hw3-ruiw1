57e9eece19b5b682d31245816303653cfc12ff12
amaliujia <amaliujia@163.com>
2014-10-13 00:51:10 -0400

    add word analyzer

--- src/main/java/annotators/DocumentVectorAnnotator.java
+++ src/main/java/annotators/DocumentVectorAnnotator.java
@@ -1,14 +1,20 @@
 package annotators;
 
-
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
+import org.apache.axis.Version;
 import org.apache.axis.wsdl.symbolTable.Utils;
+import org.apache.lucene.analysis.Analyzer.TokenStreamComponents;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -26,11 +32,13 @@ import typesystems.*;;
 
 public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 
+  public static EnglishAnalyzerConfigurable analyzer;
   private String stopwordsFile;
   private HashSet<String> stopwords;
   public void initialize(UimaContext aContext){
     stopwordsFile = (String)aContext.getConfigParameterValue("stopwords");
     stopwords = new HashSet<String>();
+    analyzer = new EnglishAnalyzerConfigurable (org.apache.lucene.util.Version.LUCENE_40);
     try {
       File file = new File(stopwordsFile);
       Scanner scanner = new Scanner(file);
@@ -67,9 +75,16 @@ public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 		String[] tokens = docText.split(" ");
 		ArrayList<Token> tempList = new ArrayList<Token>();//(ArrayList<Token>)JCasUtil.select(doc.getTokenList(), Token.class);
 		for(int i = 0; i < tokens.length; i++){
-		  if(stopwords.contains(tokens[i])){
-		    continue;
-		  }
+//		  if(stopwords.contains(tokens[i])){
+//		    continue;
+//		  }
+		  try {
+        String c[] = tokenizeQuery(tokens[i]);
+        tokens[i] = c[0];
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
 		  
 		  for(int j = 0; j < tempList.size(); j++){
 		    if(tempList.get(j).getText().equals(tokens[j])){
@@ -86,5 +101,21 @@ public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
 		}
 	  doc.setTokenList(utils.Utils.fromCollectionToFSList(jcas, tempList));
 	}
+	
+  public String[] tokenizeQuery(String query) throws IOException {
+
+    TokenStreamComponents comp = analyzer.createComponents("dummy", new StringReader(query));
+    TokenStream tokenStream = comp.getTokenStream();
+
+    CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+    tokenStream.reset();
+
+    List<String> tokens = new ArrayList<String>();
+    while (tokenStream.incrementToken()) {
+      String term = charTermAttribute.toString();
+      tokens.add(term);
+    }
+    return tokens.toArray(new String[tokens.size()]);
+  }
 
 }
